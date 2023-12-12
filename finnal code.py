e1a3458936490f2b5f3097e597bc97231982d928
Yuankun12 <yli604@jh.edu>
2023-12-12 22:50:50 +0800

    code
    1

--- finnal code.py
+++ finnal code.py
@@ -9,8 +9,10 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import Label
 from tkinter.scrolledtext import ScrolledText
+from difflib import get_close_matches
 import ttkbootstrap as ttk
 from ttkbootstrap import Style
+from fuzzywuzzy import process
 
 
 def create_rating_matrix(data, num_users, num_movies):
@@ -44,7 +46,7 @@ def evaluate_collaborative_filtering(train_data_matrix, test_data_matrix, simila
     Returns:
         tuple: Tuple containing User-Based RMSE, Item-Based RMSE, and Model-Based RMSE.
     """
-    # User-product collaborative filtering
+    # 用户-产品协同过滤
     user_similarity = pairwise_distances(train_data_matrix, metric=similarity_metric)
     item_similarity = pairwise_distances(train_data_matrix.T, metric=similarity_metric)
 
@@ -54,7 +56,7 @@ def evaluate_collaborative_filtering(train_data_matrix, test_data_matrix, simila
     user_based_rmse = rmse(user_prediction, test_data_matrix)
     item_based_rmse = rmse(item_prediction, test_data_matrix)
 
-    # Model-based collaborative filtering
+    # 基于模型的协同过滤
     u, s, vt = svds(train_data_matrix, k=model_k)
     s_diag_matrix = np.diag(s)
     model_based_prediction = np.dot(np.dot(u, s_diag_matrix), vt)
@@ -124,8 +126,8 @@ data = pd.read_csv('data/ml-100k/u.data', sep='\t', names=['userId', 'movieId', 
 data = data.drop_duplicates()
 
 # Handle missing values, if any
-print(data.isnull().sum())  # 检查缺失值
-data = data.dropna()  # 删除包含缺失值的行
+print(data.isnull().sum())  # Check for missing values
+data = data.dropna()  # Deletes rows that contain missing values
 
 # Handle rating range errors, for example, assuming that the rating should be between 1 and 5
 data['rating'] = np.clip(data['rating'], 1, 5)
@@ -134,8 +136,8 @@ data['rating'] = np.clip(data['rating'], 1, 5)
 unique_users = data['userId'].nunique()
 unique_movies = data['movieId'].nunique()
 
-print("\nNumber of unique users:", unique_users)
-print("Number of unique movies:", unique_movies)
+print("\n唯一用户数量:", unique_users)
+print("唯一电影数量:", unique_movies)
 
 # Split the data set into a training set and a test set
 train_data, test_data = train_test_split(data, test_size=0.25, random_state=42)
@@ -151,204 +153,3 @@ evaluate_collaborative_filtering(train_data_matrix, test_data_matrix)
 movies = pd.read_csv('data/ml-100k/u.item', sep='|', encoding='latin-1', header=None, names=['movieId', 'title'],
                      usecols=[0, 1])
 
-
-def get_movie_title(movie_id):
-    """
-    Get the movie title based on the given movie ID.
-
-    Parameters:
-        movie_id (int): Movie ID.
-
-    Returns:
-        str: Movie title.
-    """
-    # 根据电影ID获取电影标题
-    return movies[movies['movieId'] == movie_id]['title'].values[0]
-
-
-def get_movie_id(movie_title):
-    """
-    Get the movie ID based on the given movie title.
-
-    Parameters:
-        movie_title (str): Title of the movie.
-
-    Returns:
-        int: Movie ID if found, otherwise -1.
-    """
-    movie = movies[movies['title'] == movie_title]
-    if not movie.empty:
-        return movie['movieId'].values[0]
-    else:
-        print(f"Movie '{movie_title}' not found.")
-        return -1
-
-
-def recommend_movies_user_based(user_id, user_prediction, num_recommendations=5):
-    """
-    Recommend movies to a user using model-based collaborative filtering.
-
-    Parameters:
-        user_id (int): User ID.
-        model_based_prediction (np.ndarray): Predicted model-based ratings.
-        num_recommendations (int): Number of recommendations to provide (default: 5).
-    """
-    # Get the unrated movie forecast rating of the user
-    user_ratings = user_prediction[user_id - 1]
-    unrated_movies = np.where(train_data_matrix[user_id - 1] == 0)[0]
-
-    # Sort the unrated movies and get the top N recommended movies
-    recommendations = sorted(list(zip(unrated_movies, user_ratings[unrated_movies])), key=lambda x: x[1], reverse=True)[
-                      :num_recommendations]
-
-    if not recommendations:
-        print(f"\nUser {user_id} has rated all movies.")
-    else:
-        print(f"\nTop {num_recommendations} Movie Recommendations for User {user_id}:")
-        for movie_id, rating in recommendations:
-            print(f"{get_movie_title(movie_id + 1)} (MovieID: {movie_id + 1}, Predicted Rating: {rating:.2f})")
-
-
-def recommend_movies_model_based(user_id, model_based_prediction, num_recommendations=5):
-    """
-    Recommend movies to a user using model-based collaborative filtering.
-
-    Parameters:
-        user_id (int): User ID.
-        model_based_prediction (np.ndarray): Predicted model-based ratings.
-        num_recommendations (int): Number of recommendations to provide (default: 5).
-    """
-    # Get the unrated movie forecast rating of the user
-    user_ratings = model_based_prediction[user_id - 1]
-    unrated_movies = np.where(user_ratings == 0)[0]
-
-    # Sort the unrated movies and get the top N recommended movies
-    recommendations = sorted(list(zip(unrated_movies, user_ratings[unrated_movies])), key=lambda x: x[1], reverse=True)[
-                      :num_recommendations]
-
-    print(f"\nTop {num_recommendations} Movie Recommendations for User {user_id}:")
-    for movie_id, rating in recommendations:
-        print(f"{get_movie_title(movie_id + 1)} (MovieID: {movie_id + 1}, Predicted Rating: {rating:.2f})")
-
-
-# Model testing: Recommendation using user-product collaborative filtering
-user_id = 1  # Change to the user ID you want to generate recommendations for
-user_similarity = pairwise_distances(train_data_matrix, metric='cosine')
-user_prediction = predict_user_based(train_data_matrix, user_similarity)
-recommend_movies_user_based(user_id, user_prediction)
-
-# Model testing: Recommendations using model-based collaborative filtering
-user_id = 1  # Change to the user ID you want to generate recommendations for
-u, s, vt = svds(train_data_matrix, k=20)
-s_diag_matrix = np.diag(s)
-model_based_prediction = np.dot(np.dot(u, s_diag_matrix), vt)
-recommend_movies_model_based(user_id, model_based_prediction)
-
-
-def recommend_movies_for_movie(movie_title, model_based_prediction, num_recommendations=10):
-    """
-    Recommend movies based on the similarity to a given movie.
-
-    Parameters:
-        movie_title (str): Title of the movie.
-        model_based_prediction (np.ndarray): Predicted model-based ratings.
-        num_recommendations (int): Number of recommendations to provide (default: 10).
-
-    Returns:
-        list: List of movie recommendations.
-    """
-    movie_id = get_movie_id(movie_title)
-
-    if movie_id != -1:
-        movie_ratings = model_based_prediction[:, movie_id - 1]
-
-        # Sort the movies and get the top N recommended movies
-        recommendations = sorted(list(enumerate(movie_ratings)), key=lambda x: x[1], reverse=True)[:num_recommendations]
-
-        print(f"\nTop {num_recommendations} Movie Recommendations for Movie '{movie_title}':")
-        for movie_id, rating in recommendations:
-            print(f"{get_movie_title(movie_id + 1)} (MovieID: {movie_id + 1}, Predicted Rating: {rating:.2f})")
-
-        return recommendations
-    else:
-        print(f"Movie '{movie_title}' not found.")
-        return []
-
-
-class MovieRecommendationApp:
-    def __init__(self, root, unique_users, unique_movies, user_based_rmse, item_based_rmse, model_based_rmse):
-        """
-        Initialize the Movie Recommendation App.
-
-        Parameters:
-            root (tk.Tk): Tkinter root window.
-            unique_users (int): Number of unique users.
-            unique_movies (int): Number of unique movies.
-            user_based_rmse (float): User-Based RMSE.
-            item_based_rmse (float): Item-Based RMSE.
-            model_based_rmse (float): Model-Based RMSE.
-        """
-        self.root = root
-        self.root.title("Movie Recommendation")
-
-        # Create ttkbootstrap style
-        style = Style(theme='lumen')
-        style.configure('.', font=('Helvetica', 12))
-
-        # User input and button frame
-        input_frame = ttk.Frame(root, padding=10)
-        input_frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-
-        self.label = ttk.Label(input_frame, text="Enter the name of a movie for recommendations:")
-        self.label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-
-        self.entry = ttk.Entry(input_frame, width=40)
-        self.entry.grid(row=0, column=1, padx=5, pady=5, sticky="w")
-
-        self.button = ttk.Button(input_frame, text="Get Recommendations", command=self.show_recommendations)
-        self.button.grid(row=0, column=2, padx=5, pady=5, sticky="w")
-
-        # Create the result text box
-        frame_result = ttk.Frame(root, padding=10)
-        frame_result.grid(row=1, column=0, sticky="w")
-
-        # Resulting text box style (ScrolledText represents scrollable text)
-        self.result_text = ScrolledText(frame_result, wrap=tk.WORD, height=12, width=88)
-        self.result_text.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-
-        # Display information frame
-        frame_info = ttk.Frame(root, padding=10)
-        frame_info.grid(row=2, column=0, sticky="sw")
-
-        # Display information frame
-        ttk.Label(frame_info, text=f"Unique Users: {unique_users}").grid(row=0, column=0, sticky="w")
-        ttk.Label(frame_info, text=f"Unique Movies: {unique_movies}").grid(row=1, column=0, sticky="w")
-        ttk.Label(frame_info, text=f"User-Based RMSE: {user_based_rmse:.4f}").grid(row=2, column=0, sticky="w")
-        ttk.Label(frame_info, text=f"Item-Based RMSE: {item_based_rmse:.4f}").grid(row=3, column=0, sticky="w")
-        ttk.Label(frame_info, text=f"Model-Based RMSE: {model_based_rmse:.4f}").grid(row=4, column=0, sticky="w")
-
-    def show_recommendations(self):
-        """
-        Display movie recommendations based on user input.
-        """
-        input_movie_title = self.entry.get().strip()
-        recommendations = recommend_movies_for_movie(input_movie_title, model_based_prediction)
-
-        if recommendations:
-            result_text = "\n".join(
-                [f"{get_movie_title(movie_id + 1)} (MovieID: {movie_id + 1}, Predicted Rating: {rating:.2f})"
-                 for movie_id, rating in recommendations])
-            self.result_text.delete(1.0, tk.END)
-            self.result_text.insert(tk.END, f"Top Movie Recommendations for '{input_movie_title}':\n{result_text}")
-        else:
-            self.result_text.delete(1.0, tk.END)
-            self.result_text.insert(tk.END, f"No recommendations found for movie '{input_movie_title}'")
-
-
-user_based_rmse, item_based_rmse, model_based_rmse = evaluate_collaborative_filtering(train_data_matrix,
-                                                                                      test_data_matrix)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = MovieRecommendationApp(root, unique_users, unique_movies, user_based_rmse, item_based_rmse, model_based_rmse)
-    root.mainloop()
